673a6ba267c7a3e9fd0a9ddaac7f69b284b6e382
Denis Reznik <reznik.d.o12@gmail.com>
2024-11-21 00:12:16 +0200

    add solution for lab08

--- Reznik-Lab07-V7/Reznik-Lab07-V7/Reznik-Lab07-V7.cpp
+++ Reznik-Lab07-V7/Reznik-Lab07-V7/Reznik-Lab07-V7.cpp
@@ -13,76 +13,77 @@
 //-Виводить на екран  всі книги  які були надруковані в заданому році.
 
 
-#include <iostream>
-#include <algorithm>
-#include <ctime>     // Підключаємо бібліотеку для роботи з функцією time().
-using namespace std; // Використовуємо простір імен std для спрощення запису.
+#include <iostream> // Підключаємо бібліотеку для вводу/виводу в консоль.
+#include <algorithm> // Підключаємо бібліотеку для роботи з алгоритмами, такими як sort().
+using namespace std; // Використовуємо простір імен std для спрощення запису, щоб не писати "std::" перед кожною стандартною функцією або класом.
 
-struct Date {
-    string month;
-    int year;
+struct Date { // Оголошуємо структуру для зберігання інформації про місяць і рік.
+    string month; // Змінна для місяця (рядок).
+    int year;     // Змінна для року (ціле число).
 };
 
-struct BOOK {
-    string Name;
-    string Author;
-    struct Date date_print;
-    int Cost;
+struct BOOK {  // Оголошуємо структуру для зберігання інформації про книгу.
+    string Name;         // Змінна для збереження назви книги.
+    string Author;       // Змінна для збереження автора книги.
+    struct Date date_print; // Структура Date, що зберігає місяць і рік друку книги.
+    int Cost;            // Змінна для вартості книги.
 };
 
+// Функція для порівняння книг за назвою в алфавітному порядку.
 bool compareByAuthorName(const BOOK& a, const BOOK& b) {
-    return a.Name < b.Name;  // Порівнюємо імена по зростанню
+    return a.Name < b.Name;  // Порівнюємо імена книг по зростанню.
 }
 
+// Функція для фільтрації книг за роком видання.
 void filterByYear(BOOK *SHOP, int N, int year_book) {
-    cout << "Books printed in " << year_book << ":\n";
-    for (int i = 0; i < N; ++i) {
-        if (SHOP[i].date_print.year >= year_book) {
-            cout << "Name: " << SHOP[i].Name << ", Author: " << SHOP[i].Author
+    cout << "Books printed in " << year_book << ":\n"; // Виводимо заголовок, який вказує на рік для фільтрації.
+    for (int i = 0; i < N; ++i) { // Ітерація по кожному елементу масиву SHOP.
+        if (SHOP[i].date_print.year >= year_book) { // Якщо рік видання книги більше або дорівнює введеному року.
+            cout << "Name: " << SHOP[i].Name << ", Author: " << SHOP[i].Author // Виводимо інформацію про книгу.
                 << ", Year: " << SHOP[i].date_print.year << ", Month: " << SHOP[i].date_print.month
-                << ", Cost: " << SHOP[i].Cost << endl;
+                << ", Cost: " << SHOP[i].Cost << endl; // Виводимо рік, місяць і вартість книги.
         }
     }
 }
 
-
-int main()
+int main() // Початок головної функції програми.
 {
-    srand(time(0)); // Ініціалізуємо генератор випадкових чисел поточним часом.
+    int N = 0;  // Змінна для зберігання кількості книг.
 
-    int N = 0;
+    cout << "How many books do you want to enter" << endl; // Запитуємо користувача, скільки книг він хоче ввести.
+    cin >> N;  // Зчитуємо кількість книг, які потрібно ввести.
 
-    cout << "How many books do you want to enter" << endl; 
-    cin >> N;
+    BOOK* SHOP = new BOOK[N];  // Виділяємо динамічну пам'ять для масиву книг з розміром N.
 
-    BOOK *SHOP = new BOOK[N];
-
+    // Заповнення даних для кожної книги.
     for (int i = 0; i < N; ++i) {
         cout << "Enter book name ";
-        cin >> SHOP[i].Name;
+        cin >> SHOP[i].Name;  // Вводимо назву книги.
         cout << "Enter book author ";
-        cin >> SHOP[i].Author;
+        cin >> SHOP[i].Author; // Вводимо автора книги.
         cout << "Enter month of printing book ";
-        cin >> SHOP[i].date_print.month;
+        cin >> SHOP[i].date_print.month; // Вводимо місяць друку книги.
         cout << "Enter year of printing book ";
-        cin >> SHOP[i].date_print.year;
+        cin >> SHOP[i].date_print.year;  // Вводимо рік друку книги.
         cout << "Enter cost of book ";
-        cin >> SHOP[i].Cost;
+        cin >> SHOP[i].Cost;  // Вводимо вартість книги.
         cout << endl;
     }
 
-    sort(SHOP, SHOP + N, compareByAuthorName);
-    int year_book;
+    sort(SHOP, SHOP + N, compareByAuthorName);  // Сортуємо масив SHOP за назвою книги в алфавітному порядку за допомогою алгоритму sort.
 
-    cout << "Please, enter year of books you want to see" << endl;
-    cin >> year_book;
+    int year_book;  // Змінна для зберігання введеного року для фільтрації.
 
-    filterByYear(SHOP, N, year_book);
+    cout << "Please, enter year of books you want to see" << endl; // Запитуємо користувача, книги якого року він хоче побачити.
+    cin >> year_book;  // Зчитуємо рік для фільтрації.
 
-    delete[] SHOP;
+    filterByYear(SHOP, N, year_book);  // Викликаємо функцію для фільтрації книг за роком видання.
 
-    return 0;
+    delete[] SHOP;  // Звільняємо пам'ять, виділену для масиву SHOP.
+
+    return 0;  // Завершуємо програму з успішним статусом.
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
---
+++ Reznik-Lab08-V7/Reznik-Lab08-V7/Reznik-Lab08-V7.cpp
@@ -0,0 +1,58 @@
+﻿// Reznik-Lab08-V7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// author Reznik D.O. student of group 222z (online)
+
+//Написати функцію, що для квадратного рівняння із заданими коефіцієнтами, повертає значення дискримінант 
+//та обчислює корені квадратного рівняння із заданими коефіцієнтами і повертає їх за допомогою 
+//параметрів - вказівників та через зовнішні змінні.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double solveQuadraticEquation(double a, double b, double c, double* root1, double* root2) {
+    double D = b * b - 4 * a * c;
+
+    if (D < 0) {
+        cout << "No real roots." << endl;
+        return D;
+    }
+
+    if (D == 0) {
+        *root1 = -b / (2 * a);
+        *root2 = *root1;
+    }
+    else {
+        *root1 = (-b + sqrt(D)) / (2 * a);
+        *root2 = (-b - sqrt(D)) / (2 * a);
+    }
+
+    return D;
+}
+
+int main() {
+    double a, b, c;
+    double root1, root2;
+
+    cout << "Enter coefficients a, b, c for the quadratic equation (ax^2 + bx + c = 0): " << endl;
+    cin >> a >> b >> c;
+
+    if (a == 0) {
+        cout << "The coefficient 'a' must be non-zero for a quadratic equation." << endl;
+        return 1;
+    }
+
+    double D = solveQuadraticEquation(a, b, c, &root1, &root2);
+
+    cout << "Discriminant (D): " << D << endl;
+
+    if (D >= 0) {
+        cout << "Root 1: " << root1 << endl;
+        cout << "Root 2: " << root2 << endl;
+    }
+
+    return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
