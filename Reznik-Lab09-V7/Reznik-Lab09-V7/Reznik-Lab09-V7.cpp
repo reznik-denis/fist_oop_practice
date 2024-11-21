0877e2f866b53eef852842ad3a65b2722f8f335d
Denis Reznik <reznik.d.o12@gmail.com>
2024-11-21 23:17:19 +0200

    add solution lab10

--- Reznik-Lab09-V7/Reznik-Lab09-V7/Reznik-Lab09-V7.cpp
+++ Reznik-Lab09-V7/Reznik-Lab09-V7/Reznik-Lab09-V7.cpp
@@ -14,98 +14,60 @@
 //для розв’язку наступної задачі запис структури в текстовий файл та считування данних з текстового файлу.
 
 
-#include <iostream> // Підключаємо бібліотеку для вводу/виводу в консоль.
-#include <algorithm> // Підключаємо бібліотеку для роботи з алгоритмами, такими як sort().
-#include <string>
-#include <fstream>
-using namespace std; // Використовуємо простір імен std для спрощення запису, щоб не писати "std::" перед кожною стандартною функцією або класом.
+#include <iostream> // Підключаємо бібліотеку для роботи з введенням і виведенням у консоль (cin, cout, endl).
+#include <algorithm> // Бібліотека для роботи з алгоритмами, такими як sort(), find(), і т. д.
+#include <string> // Бібліотека для роботи з рядками типу std::string.
+#include <fstream> // Бібліотека для роботи з файлами (читання та запис).
+using namespace std; // Директива, що дозволяє не писати "std::" перед стандартними об'єктами та функціями.
 
-struct Date { // Оголошуємо структуру для зберігання інформації про місяць і рік.
-    string month; // Змінна для місяця (рядок).
-    int year;     // Змінна для року (ціле число).
+struct Date { // Оголошення структури для збереження дати друку книги.
+    string month; // Змінна для місяця друку (назва місяця у вигляді рядка, наприклад, "January").
+    int year; // Змінна для року друку (ціле число, наприклад, 2023).
 };
 
-struct BOOK {  // Оголошуємо структуру для зберігання інформації про книгу.
-    string Name;         // Змінна для збереження назви книги.
-    string Author;       // Змінна для збереження автора книги.
-    struct Date date_print; // Структура Date, що зберігає місяць і рік друку книги.
-    int Cost;            // Змінна для вартості книги.
+struct BOOK { // Оголошення структури для збереження інформації про книгу.
+    string Name; // Назва книги.
+    string Author; // Ім'я автора книги.
+    struct Date date_print; // Структура Date для збереження дати друку книги.
+    int Cost; // Ціна книги у вигляді цілого числа.
 };
 
 bool compareByAuthorName(const BOOK& a, const BOOK& b) {
-    return a.Name < b.Name;
+    return a.Name < b.Name; // Функція порівнює назви книг для сортування у порядку зростання.
 }
 
-//void writeToFile(BOOK* group, int N, const string& filename) {
-//    ofstream file(filename);
-//    if (file.is_open()) {
-//        for (int i = 0; i < N; ++i) {
-//            file << i + 1 << ". Name: " << group[i].Name
-//                << ", Author: " << group[i].Author
-//                << ", Date publication: " << group[i].date_print.month
-//                << " " << group[i].date_print.year
-//                << " year, Cost:" << group[i].Cost;
-// 
-//            file << endl;
-//        }
-//        file.close();
-//        cout << "Data write success!" << filename << endl;
-//    }
-//    else {
-//        cout << "Error opening file!" << endl;
-//    }
-//}
 void writeToFile(BOOK* group, int N, const string& filename) {
-    ofstream file(filename);
-    if (file.is_open()) {
-        file << N << endl; // Запис кількості книг
-        for (int i = 0; i < N; ++i) {
-            file << group[i].Name << "|"
-                << group[i].Author << "|"
-                << group[i].date_print.month << "|"
-                << group[i].date_print.year << "|"
-                << group[i].Cost << endl; // Використовуємо "|" як роздільник
+    ofstream file(filename); // Відкриваємо файл для запису.
+    if (file.is_open()) { // Перевіряємо, чи файл відкритий успішно.
+        file << N << endl; // Записуємо кількість книг у файл.
+        for (int i = 0; i < N; ++i) { // Перебираємо масив книг.
+            file << group[i].Name << "|" // Записуємо назву книги та роздільник.
+                << group[i].Author << "|" // Записуємо автора та роздільник.
+                << group[i].date_print.month << "|" // Записуємо місяць друку.
+                << group[i].date_print.year << "|" // Записуємо рік друку.
+                << group[i].Cost << endl; // Записуємо вартість книги.
         }
-        file.close();
-        cout << "Data write success! File: " << filename << endl;
+        file.close(); // Закриваємо файл після запису.
+        cout << "Data write success! File: " << filename << endl; // Повідомлення про успішний запис.
     }
     else {
-        cout << "Error opening file!" << endl;
+        cout << "Error opening file!" << endl; // Помилка відкриття файлу.
     }
 }
 
-
-//void readFromFile(BOOK*& group, int& N, const string& filename) {
-//    ifstream file(filename);
-//    if (file.is_open()) {
-//        file >> N;
-//        delete[] group;
-//        group = new BOOK[N];
-//        for (int i = 0; i < N; ++i) {
-//            file >> group[i].Name >> group[i].Author 
-//                >> group[i].date_print.month 
-//                >> group[i].date_print.year >> group[i].Cost;
-//        }
-//        file.close();
-//        cout << "Data read success!" << filename << endl;
-//    }
-//    else {
-//        cout << "Error opening file!" << endl;
-//    }
-//}
 void readFromFile(BOOK*& group, int& N, const string& filename) {
-    ifstream file(filename);
-    if (file.is_open()) {
-        file >> N; // Зчитуємо кількість книг
-        file.ignore(); // Пропускаємо символ нового рядка після N
-        delete[] group;
-        group = new BOOK[N];
+    ifstream file(filename); // Відкриваємо файл для читання.
+    if (file.is_open()) { // Перевіряємо, чи файл відкрився успішно.
+        file >> N; // Зчитуємо кількість книг із файлу.
+        file.ignore(); // Пропускаємо символ нового рядка.
+        delete[] group; // Звільняємо пам'ять, якщо масив уже існував.
+        group = new BOOK[N]; // Виділяємо нову пам'ять для книг.
         for (int i = 0; i < N; ++i) {
             string line;
-            getline(file, line); // Зчитуємо весь рядок
-            size_t pos = 0;
+            getline(file, line); // Зчитуємо весь рядок із файлу.
+            size_t pos = 0; // Індикатор для пошуку роздільників.
 
-            // Парсимо дані за допомогою роздільника "|"
+            // Розбиваємо рядок на частини (назва, автор, дата, вартість).
             pos = line.find("|");
             group[i].Name = line.substr(0, pos);
             line.erase(0, pos + 1);
@@ -122,49 +84,47 @@ void readFromFile(BOOK*& group, int& N, const string& filename) {
             group[i].date_print.year = stoi(line.substr(0, pos));
             line.erase(0, pos + 1);
 
-            group[i].Cost = stoi(line); // Решта - це вартість
+            group[i].Cost = stoi(line); // Зчитуємо вартість книги.
         }
-        file.close();
-        cout << "Data read success! File: " << filename << endl;
+        file.close(); // Закриваємо файл після зчитування.
+        cout << "Data read success! File: " << filename << endl; // Повідомлення про успішне зчитування.
     }
     else {
-        cout << "Error opening file!" << endl;
+        cout << "Error opening file!" << endl; // Помилка відкриття файлу.
     }
 }
 
-
 void filterByYear(BOOK* SHOP, int N, int year_book) {
-    cout << "Books printed in " << year_book << ":\n"; 
-    for (int i = 0; i < N; ++i) { 
-        if (SHOP[i].date_print.year >= year_book) { 
-            cout << "Name: " << SHOP[i].Name << ", Author: " << SHOP[i].Author 
+    cout << "Books printed in " << year_book << ":\n"; // Виводимо заголовок для фільтру.
+    for (int i = 0; i < N; ++i) { // Перебираємо всі книги.
+        if (SHOP[i].date_print.year >= year_book) { // Якщо рік друку більший або дорівнює заданому.
+            cout << "Name: " << SHOP[i].Name << ", Author: " << SHOP[i].Author
                 << ", Year: " << SHOP[i].date_print.year << ", Month: " << SHOP[i].date_print.month
-                << ", Cost: " << SHOP[i].Cost << endl;
+                << ", Cost: " << SHOP[i].Cost << endl; // Виводимо дані про книгу.
         }
     }
 }
 
 void displayBooks(BOOK* group, int n) {
-    cout << "All books in memory:" << endl;
-    for (int i = 0; i < n; ++i) {
+    cout << "All books in memory:" << endl; // Заголовок для списку книг.
+    for (int i = 0; i < n; ++i) { // Перебираємо масив книг.
         cout << i + 1 << ". Name: " << group[i].Name
             << ", Author: " << group[i].Author
-            << ", Date publication: " << group[i].date_print.month 
-            << " " << group[i].date_print.year 
-            << " year, Cost:" << group[i].Cost;
-        
-        cout << endl;
+            << ", Date publication: " << group[i].date_print.month
+            << " " << group[i].date_print.year
+            << " year, Cost:" << group[i].Cost; // Виводимо деталі кожної книги.
+
+        cout << endl; // Переходимо до нового рядка після кожної книги.
     }
 }
 
-int main()
-{
-    int N = 0;  
-    BOOK* SHOP = nullptr;  
-    string filename = "shopCatalog.txt";
+int main() {
+    int N = 0; // Змінна для кількості книг.
+    BOOK* SHOP = nullptr; // Динамічний масив книг, спочатку порожній.
+    string filename = "shopCatalog.txt"; // Назва файлу для запису/читання.
 
-    while (true) {
-        cout << "\nMenu:\n";
+    while (true) { // Нескінченний цикл для роботи меню.
+        cout << "\nMenu:\n"; // Виводимо меню.
         cout << "1. Enter books\n";
         cout << "2. Show all books\n";
         cout << "3. Show books by year\n";
@@ -174,59 +134,58 @@ int main()
         cout << "Choose option: ";
 
         int choice;
-        cin >> choice;
+        cin >> choice; // Зчитуємо вибір користувача.
 
-        switch (choice) {
-        case 1: {
+        switch (choice) { // Обробляємо вибір за допомогою конструкції switch.
+        case 1: { // Введення книг.
             cout << "How many books do you want to enter" << endl;
             cin >> N;
-            delete[] SHOP;
-            SHOP = new BOOK[N];
+            delete[] SHOP; // Очищуємо пам'ять для попередніх книг.
+            SHOP = new BOOK[N]; // Виділяємо пам'ять для нового масиву.
 
-            for (int i = 0; i < N; ++i) {
+            for (int i = 0; i < N; ++i) { // Зчитуємо дані для кожної книги.
                 cout << "Enter book name ";
-                cin.ignore(); // Очищаємо буфер після попереднього вводу
+                cin.ignore(); // Очищаємо буфер після попереднього вводу.
                 getline(cin, SHOP[i].Name);
                 cout << "Enter book author ";
                 getline(cin, SHOP[i].Author);
                 cout << "Enter month of printing book ";
-                cin >> SHOP[i].date_print.month; 
+                cin >> SHOP[i].date_print.month;
                 cout << "Enter year of printing book ";
-                cin >> SHOP[i].date_print.year;  
+                cin >> SHOP[i].date_print.year;
                 cout << "Enter cost of book ";
-                cin >> SHOP[i].Cost; 
+                cin >> SHOP[i].Cost;
                 cout << endl;
             }
 
-            sort(SHOP, SHOP + N, compareByAuthorName);
-
+            sort(SHOP, SHOP + N, compareByAuthorName); // Сортуємо книги за назвою.
             break;
         }
         case 2:
-            displayBooks(SHOP, N);
+            displayBooks(SHOP, N); // Виведення всіх книг.
             break;
         case 3:
             int year_book;
             cout << "Please, enter year of books you want to see" << endl;
             cin >> year_book;
-            filterByYear(SHOP, N, year_book);
+            filterByYear(SHOP, N, year_book); // Фільтруємо книги за роком.
             break;
         case 4:
-            writeToFile(SHOP, N, filename);
+            writeToFile(SHOP, N, filename); // Запис книг у файл.
             break;
         case 5:
-            readFromFile(SHOP, N, filename);
+            readFromFile(SHOP, N, filename); // Читання книг із файлу.
             break;
         case 6:
-            delete[] SHOP;
+            delete[] SHOP; // Очищення пам'яті перед завершенням програми.
             cout << "Exit out." << endl;
-            return 0;
+            return 0; // Завершення програми.
         default:
-            cout << "Wrong choose. Try again." << endl;
+            cout << "Wrong choose. Try again." << endl; // Повідомлення про помилковий вибір.
         }
     }
 
-    return 0;
+    return 0; // Завершення функції main.
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
---
+++ Reznik-Lab10-V7/Reznik-Lab10-V7/Reznik-Lab10-V7.cpp
@@ -0,0 +1,148 @@
+﻿// Reznik-Lab10-V7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// author Reznik D.O. student of group 222z (online)
+
+//Варіант 7.
+//Написати клас  BOOK, яка містить  наступні поля :
+//-Name  – назва книги;
+//-Avtor – автори книги;
+//-Data –дата друку;
+//-Cost - ціна книги.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BOOK {
+private:
+    string Name;
+    string Author;
+    string Date;
+    double Cost;
+
+public:
+    BOOK() : Name(""), Author(""), Date(""), Cost(0.0) {}
+    BOOK(string name, string author, string date, double cost)
+        : Name(name), Author(author), Date(date), Cost(cost) {
+    }
+    ~BOOK() {
+        cout << "Destructor called for book: " << Name << endl;
+    }
+    void setName(const string& name) {
+        Name = name;
+    }
+    string getName() const {
+        return Name;
+    }
+    void setAuthor(const string& author) {
+        Author = author;
+    }
+    string getAuthor() const {
+        return Author;
+    }
+    void setDate(const string& date) {
+        Date = date;
+    }
+    string getDate() const {
+        return Date;
+    }
+    void setCost(double cost) {
+        Cost = cost;
+    }
+    double getCost() const {
+        return Cost;
+    }
+    void inputInfo() {
+        cout << "Enter book name: ";
+        getline(cin, Name);
+        cout << "Enter book author: ";
+        getline(cin, Author);
+        cout << "Enter publication date (MM.YYYY): ";
+        getline(cin, Date);
+        cout << "Enter book cost: ";
+        cin >> Cost;
+        cin.ignore();
+    }
+    void displayInfo() const {
+        cout << "Name: " << Name << endl;
+        cout << "Author: " << Author << endl;
+        cout << "Date: " << Date << endl;
+        cout << "Cost: $" << Cost << endl;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the number of books: ";
+    cin >> n;
+    cin.ignore();
+    BOOK* books = new BOOK[n];
+    for (int i = 0; i < n; ++i) {
+        cout << "\nBook " << i + 1 << ":" << endl;
+        books[i].inputInfo();
+    }
+    cout << "\nBooks details:" << endl;
+    for (int i = 0; i < n; ++i) {
+        cout << "\nBook " << i + 1 << ":" << endl;
+        books[i].displayInfo();
+    }
+    cout << "What number of book do you want to change?" << endl;
+    int num;
+    cin >> num;
+    
+    while (true) {
+        cout << "What value do you want to change?" << endl;
+        cout << "1. Name" << endl;
+        cout << "2. Author" << endl;
+        cout << "3. Date" << endl;
+        cout << "4. Cost" << endl;
+        cout << "5. Finish edit" << endl;
+        int choose;
+        cin >> choose;
+        switch (choose) {
+        case 1: {
+            cout << "Enter name" << endl;
+            string name;
+            cin.ignore();
+            getline(cin, name);
+            books[num].setName(name);
+            break;
+        }
+        case 2: {
+            cout << "Enter author" << endl;
+            string author;
+            cin.ignore();
+            getline(cin, author);
+            books[num].setAuthor(author);
+            break;
+        }
+        case 3: {
+            cout << "Enter date" << endl;
+            string date;
+            cin >> date;
+            books[num].setDate(date);
+            break;
+        }
+        case 4: {
+            cout << "Enter cost" << endl;
+            double cost;
+            cin >> cost;
+            books[num].setCost(cost);
+            break;
+        }
+        case 5: {
+            cout << "Exit" << endl;
+            cout << "Your new book" << endl;
+            books[num].displayInfo();
+            return 0;
+        }
+        default:
+            cout << "You enter wrong choose" << endl;
+        }
+    }
+    delete[] books;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
